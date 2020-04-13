48cf5f3b8345f29745cb48c92746ac0f45b86c00
Michael Cary <39915578+cat-astrophic@users.noreply.github.com>
2020-04-13 14:26:25 -0400

    analysis of KP on C intensities for cement manufacturing

--- cementensity.R
+++ cementensity.R
@@ -56,3 +56,55 @@ write.csv(stargazer(i1, i2, i3, i4, i5, i6, type = 'text'),
 write.csv(stargazer(i1, i2, i3, i4, i5, i6),
           paste(directory, 'intensity_regression_results_tex.txt'), row.names = FALSE)
 
+# Creating figures for the paper
+
+# Data prep -- aggregate time series for C intensities
+
+yrs <- c()
+c_int <- c()
+
+for (i in 1990:2014) {
+  
+  d <- cement[which(cement$Year == i),]
+  d <- d[which(d$Intensity > 0),]
+  d <- d[which(d$Cement > 0),]
+  c_int[[i-1989]] <- sum(d$Intensity * d$Cement) / sum(d$Cement)
+  yrs[[i-1989]] <- i
+  
+}
+
+US <- cement[which(cement$Country == 'United States'),]
+us_int <- US$Intensity
+China <- cement[which(cement$Country == 'China'),]
+china_int <- China$Intensity
+Cint.df <- data.frame(Year = c(yrs), World = c(c_int), US = c(us_int), China = c(china_int))
+
+# First plot
+
+ggplot(data = Cint.df, aes(x = Year, y = value, color = variable)) +
+  ggtitle('CO2 Emissions from Global Cement Production by Year') +
+  ylab('CO2 Emissions (million metric tons of C)') +
+  geom_line(aes(y = World, col = 'Global Average'), size = 2, alpha = 1) +
+  geom_line(aes(y = US, col = 'USA'), size = 2, alpha = 1) +
+  geom_line(aes(y = China, col = 'China'), size = 2, alpha = 1) +
+  theme(legend.position = c(0.2,0.2), plot.title = element_text(hjust = 0.5)) +
+  ylim(0.03695,0.03715) + theme(legend.title = element_blank())
+
+dev.copy(png, paste(directory, 'cementensity_fig_1.png'))
+dev.off()
+
+# Second plot -- first plot with markers for KP
+
+ggplot(data = Cint.df, aes(x = Year, y = value, color = variable)) +
+  ggtitle('Carbon Intensity for Global Cement Production by Year\nwith Kyoto Protocol Indicators') +
+  ylab('Carbon Intensity (kg of C per kg of Cement)') +
+  geom_line(aes(y = World, col = 'Global Average'), size = 2, alpha = 1) +
+  geom_line(aes(y = US, col = 'USA'), size = 2, alpha = 1) +
+  geom_line(aes(y = China, col = 'China'), size = 2, alpha = 1) +
+  theme(legend.position = c(0.2,0.2), plot.title = element_text(hjust = 0.5)) +
+  ylim(0.03695,0.03715) + theme(legend.title = element_blank()) +
+  geom_vline(xintercept = 2008) + geom_vline(xintercept = 2013)
+
+dev.copy(png, paste(directory, 'cementensity_fig_2.png'))
+dev.off()
+
