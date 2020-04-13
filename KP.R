3e884aad68cec6e67bc54dcf946c2abe4b979d95
Michael Cary <39915578+cat-astrophic@users.noreply.github.com>
2020-04-13 12:00:41 -0400

    main script

---
+++ KP.R
@@ -0,0 +1,17 @@
+# Main script for project studying the effect of the Kyoto Protocol on cement manufaction
+
+# Reading in the data -- make sure that the filepath below matches where you store the data
+
+filepath <- 'C:/Users/User/Documents/Data/cement/cement.csv'
+
+# Declaring a directory to write results to
+
+directory <- 'C:/Users/User/Documents/Data/cement/'
+
+# Running the three scripts -- ensure that the directory is correct
+
+source('C:/Users/User/Documents/cement.R')
+source('C:/Users/User/Documents/cemissions.R')
+source('C:/Users/User/Documents/cementensity.R')
+
+
