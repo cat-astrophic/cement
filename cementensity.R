130535c553b1e4db7d15c532bef2ec47020666d5
Michael Cary <39915578+cat-astrophic@users.noreply.github.com>
2020-04-13 11:20:10 -0400

    analysis of KP on C intensities for cement manufacturing

---
+++ cementensity.R
@@ -0,0 +1,62 @@
+# This script performs the data analysis for a paper on the effect of the Kyoto Protocol on cement manufacturing
+
+# Loading libraries
+
+library(stargazer)
+library(ggplot2)
+
+# Declaring the filepath for the data file
+
+filepath <- 'C:/Users/User/Documents/Data/cement/cement.csv'
+
+# Reading in the panel data
+
+cement <- read.csv(filepath, fileEncoding = 'UTF-8-BOM')
+
+# Subsetting the data in the same fashion as the previous two scripts
+
+cement <- cement[which(abs(cement$Real.Interest.Rate) < 50),]
+
+# Plotting data
+
+hist(cement$Intensity)
+plot(cement$Year,cement$Intensity)
+
+# We see that there are some extreme outliers in the above plot so we remove them -- anything above 99.5 percentile
+
+cutoff <- quantile(xxx, c(.995), na.rm = TRUE)
+cement <- cement[which(cement$Intensity <= cutoff),]
+
+# Plotting data
+
+hist(cement$Intensity)
+plot(cement$Year,cement$Intensity)
+
+# Running Carbon intensity regressions for cement manufacturing
+
+i1 <- lm(Intensity ~ Lagged.Intensity + log(GDP.per.capita) + Lagged.R.D + Real.Interest.Rate + Renewable.Electricity.Output, data = cement)
+
+i2 <- lm(Intensity ~ Lagged.Intensity + log(GDP.per.capita) + KP + Lagged.R.D + Real.Interest.Rate + Renewable.Electricity.Output, data = cement)
+
+i3 <- lm(Intensity ~ Lagged.Intensity + log(GDP.per.capita)*KP + Lagged.R.D + Real.Interest.Rate + Renewable.Electricity.Output, data = cement)
+
+i4 <- lm(Intensity ~ Lagged.Intensity + log(GDP.per.capita) + Lagged.R.D + Real.Interest.Rate + Renewable.Electricity.Output
+         + factor(Year), data = cement)
+
+i5 <- lm(Intensity ~ Lagged.Intensity + log(GDP.per.capita) + KP + Lagged.R.D + Real.Interest.Rate + Renewable.Electricity.Output
+         + factor(Year), data = cement)
+
+i6 <- lm(Intensity ~ Lagged.Intensity + log(GDP.per.capita)*KP + Lagged.R.D + Real.Interest.Rate + Renewable.Electricity.Output
+         + factor(Year), data = cement)
+
+# Viewing results
+
+stargazer(i1, i2, i3, i4, i5, i6, type = 'text')
+
+# Writing results to file
+
+write.csv(stargazer(i1, i2, i3, i4, i5, i6, type = 'text'),
+          'C:/Users/User/Documents/Data/cement/intensity_regression_results.txt', row.names = FALSE)
+write.csv(stargazer(i1, i2, i3, i4, i5, i6),
+          'C:/Users/User/Documents/Data/cement/intensity_regression_results_tex.txt', row.names = FALSE)
+
