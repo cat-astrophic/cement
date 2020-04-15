bbdf9257a58d2ff452cb8401067e27b0d82e99e3
Michael Cary <39915578+cat-astrophic@users.noreply.github.com>
2020-04-14 22:37:39 -0400

    analysis of KP on C intensities for cement manufacturing

--- cementensity.R
+++ cementensity.R
@@ -62,21 +62,21 @@ write.csv(stargazer(i1, i2, i3, i4, i5, i6),
 
 yrs <- c()
 c_int <- c()
+china_int <- c()
+us_int <- c()
 
-for (i in 1990:2014) {
+for (i in 1998:2014) {
   
   d <- cement[which(cement$Year == i),]
   d <- d[which(d$Intensity > 0),]
   d <- d[which(d$Cement > 0),]
-  c_int[[i-1989]] <- sum(d$Intensity * d$Cement) / sum(d$Cement)
-  yrs[[i-1989]] <- i
+  c_int[[i-1997]] <- sum(d$Intensity * d$Cement) / sum(d$Cement)
+  china_int[[i-1997]] <- d[which(d$Country == 'China'),58] 
+  us_int[[i-1997]] <- d[which(d$Country == 'United States'),58]
+  yrs[[i-1997]] <- i
   
 }
 
-US <- cement[which(cement$Country == 'United States'),]
-us_int <- US$Intensity
-China <- cement[which(cement$Country == 'China'),]
-china_int <- China$Intensity
 Cint.df <- data.frame(Year = c(yrs), World = c(c_int), US = c(us_int), China = c(china_int))
 
 # First plot
@@ -106,5 +106,54 @@ ggplot(data = Cint.df, aes(x = Year, y = value, color = variable)) +
   geom_vline(xintercept = 2008) + geom_vline(xintercept = 2013)
 
 dev.copy(png, paste(directory, 'cementensity_fig_2.png'))
+dev.off()
+
+# Third plot -- visualizing the relationship between income and carbon intensities -- High income countries have GDP per capita >= 40,000
+
+cement <- cement[which(cement$Cement > 0 & cement$Intensity > 0),]
+high <- cement[which(cement$GDP.per.capita >= 30000),]
+low <- cement[which(cement$GDP.per.capita > 0 & cement$GDP.per.capita < 30000),]
+
+hi <- c()
+li <- c()
+yrs <- c()
+
+for (yr in 1990:2014) {
+  
+  h <- high[which(high$Year == yr),]
+  l <- low[which(low$Year == yr),]
+  
+  hc <- sum(h$Cement)
+  lc <- sum(l$Cement)
+  
+  hi[[yr-1989]] <- sum(h$Intensity * h$Cement) / hc
+  li[[yr-1989]] <- sum(l$Intensity * l$Cement) / lc
+  yrs[[yr-1989]] <- yr
+  
+}
+
+Cint.df <- data.frame(Year = c(yrs), High = c(hi), Low = c(li))
+
+ggplot(data = Cint.df[which(Cint.df$Year < 2008),], aes(x = Year, y = value)) +
+  ggtitle('Relationship Between GDP per capita and Carbon Intensity\nPrior to the Kyoto Protocol') +
+  ylab('Carbon Intensity (kg of C per kg of Cement)') +
+  geom_line(aes(y = High, col = 'High Income Nations'), size = 2, alpha = 1) +
+  geom_line(aes(y = Low, col = 'Low Income Nations'), size = 2, alpha = 1) +
+  theme(legend.position = c(0.19,0.81), legend.title = element_blank(), plot.title = element_text(hjust = 0.5))
+
+dev.copy(png, paste(directory, 'cementensity_fig_3.png'))
+dev.off()
+
+# Fourth plot -- an extension of the thrid plot
+
+ggplot(data = Cint.df[which(Cint.df$Year < 2012),], aes(x = Year, y = value)) +
+  ggtitle('Relationship Between GDP per capita and Carbon Intensity') +
+  ylab('Carbon Intensity (kg of C per kg of Cement)') +
+  geom_line(aes(y = High, col = 'High Income Nations'), size = 2, alpha = 1) +
+  geom_line(aes(y = Low, col = 'Low Income Nations'), size = 2, alpha = 1) +
+  theme(legend.position = c(0.2075,0.425), legend.title = element_blank(), plot.title = element_text(hjust = 0.5)) +
+  geom_vline(xintercept = 2008)
+  
+dev.copy(png, paste(directory, 'cementensity_fig_4.png'))
 dev.off()
 
