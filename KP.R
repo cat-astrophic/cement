4f2fbeae11f281d558224cbf1be2976aba681874
Michael Cary <39915578+cat-astrophic@users.noreply.github.com>
2020-04-27 19:45:08 -0400

    main script

--- KP.R
+++ KP.R
@@ -2,18 +2,18 @@
 
 # Reading in the data -- make sure that the filepath below matches where you store the data
 
-filepath <- 'C:/Users/M535040/Documents/Data/cement/cement.csv'
+filepath <- 'C:/Users/User/Documents/Data/cement/cement.csv'
 
 # Declaring a directory to write results to
 
-directory <- 'C:/Users/M535040/Documents/Data/cement/'
+directory <- 'C:/Users/User/Documents/Data/cement/'
 
 # Running the three scripts -- ensure that the directory is correct
 
-source('C:/Users/M535040/Documents/cement.R')
-source('C:/Users/M535040/Documents/cement_differenced.R')
-source('C:/Users/M535040/Documents/cemissions.R')
-source('C:/Users/M535040/Documents/cemissions_differenced.R')
-source('C:/Users/M535040/Documents/cementensity.R')
-source('C:/Users/M535040/Documents/cementensity_differenced.R')
+source('C:/Users/User/Documents/cement.R')
+source('C:/Users/User/Documents/cement_differenced.R')
+source('C:/Users/User/Documents/cemissions.R')
+source('C:/Users/User/Documents/cemissions_differenced.R')
+source('C:/Users/User/Documents/cementensity.R')
+source('C:/Users/User/Documents/cementensity_differenced.R')
 
