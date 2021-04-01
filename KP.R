f441ccde92b4a8da86c0855bfc40a29f3ba9484d
Michael Cary <39915578+cat-astrophic@users.noreply.github.com>
2021-04-01 10:20:00 -0400

    main script

--- KP.R
+++ KP.R
@@ -1,4 +1,4 @@
-# Main script for project studying the effect of the Kyoto Protocol on cement manufaction
+# Main script for project studying the effect of the Kyoto Protocol on cement manufacturing
 
 # Reading in the data -- make sure that the filepath below matches where you store the data
 
@@ -7,6 +7,10 @@ filepath <- 'C:/Users/User/Documents/Data/cement/cement.csv'
 # Declaring a directory to write results to
 
 directory <- 'C:/Users/User/Documents/Data/cement/'
+
+# This is where the raw net trade data files are stored
+
+tradedir <- 'C:/Users/User/Documents/Data/cement/trade/'
 
 # Running the three scripts -- ensure that the directory is correct
 
@@ -17,4 +21,5 @@ source('C:/Users/User/Documents/cemissions_differenced.R')
 source('C:/Users/User/Documents/cementensity.R')
 source('C:/Users/User/Documents/cementensity_differenced.R')
 source('C:/Users/User/Documents/cementensity_simulation.R')
+source('C:/Users/User/Documents/cement_leakage.R')
 
